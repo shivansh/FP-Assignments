182a922bf47a131e4b2e747aae117c7fea78dc5b
Shivansh Rai <shivansh@freebsd.org>
2018-01-25 23:42:36 +0530

    q3: Increase laziness

--- ass2/q3.hs
+++ ass2/q3.hs
@@ -4,6 +4,6 @@ errno = length . unbalanced
 -- unbalanced returns a list of chars which needs to
 -- be removed to obtain a balanced parentheses string.
 unbalanced :: String -> String
-unbalanced = foldl (\acc x -> if length acc == 0 then x:acc
-                         else if head acc == '(' && x == ')' then tail acc
+unbalanced = foldl (\acc x -> if null acc then x:acc
+                         else if (head acc, x) == ('(', ')') then tail acc
                          else x:acc) []
