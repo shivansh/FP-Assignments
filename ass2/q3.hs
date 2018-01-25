15d796027e5e28d1e0e9ef760ccadeb13c583ea4
Shivansh Rai <shivansh@freebsd.org>
2018-01-25 23:22:43 +0530

    q3: Update to match expected type

--- ass2/q3.hs
+++ ass2/q3.hs
@@ -1,7 +1,9 @@
--- errno returns a list of chars which need to be removed
--- to obtain a well balanced parentheses string (WBPS).
--- TODO: Change signature to String -> Int
-errno :: String -> String
-errno = foldl (\acc x -> if length acc == 0 then x:acc
+errno :: String -> Int
+errno = length . unbalanced
+
+-- unbalanced returns a list of chars which needs to
+-- be removed to obtain a balanced parentheses string.
+unbalanced :: String -> String
+unbalanced = foldl (\acc x -> if length acc == 0 then x:acc
                          else if head acc == '(' && x == ')' then tail acc
                          else x:acc) []
