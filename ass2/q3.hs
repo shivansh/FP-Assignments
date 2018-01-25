7a84eb58d422b5da7a3e8fde427ee2dd30dbe982
Shivansh Rai <shivansh@freebsd.org>
2018-01-25 23:20:43 +0530

    q3: Optimize errno

--- ass2/q3.hs
+++ ass2/q3.hs
@@ -2,17 +2,6 @@
 -- to obtain a well balanced parentheses string (WBPS).
 -- TODO: Change signature to String -> Int
 errno :: String -> String
-errno = foldl (\acc x ->
-                if x == '(' then acc ++ [x]
-                else if length acc == 0 then acc ++ [x]
-                else if last acc == '(' then init acc
-                else acc ++ [x]) []
-
-errno' :: String -> Int
-errno' xs
-  | xs == [] = 0
-  | head xs == '(' && last xs == ')' = errno' $ init $ tail xs
-  | otherwise = 1 + min (errno' (init xs)) (errno' (tail xs))
-
--- (()
--- ))())
+errno = foldl (\acc x -> if length acc == 0 then x:acc
+                         else if head acc == '(' && x == ')' then tail acc
+                         else x:acc) []
