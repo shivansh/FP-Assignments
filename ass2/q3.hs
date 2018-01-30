29c08cbf495a2b0784f7cd920d34308e44863178
Shivansh Rai <shivansh@freebsd.org>
2018-01-30 19:37:05 +0530

    q3: Replace guards with pattern matching (trance acquired)

--- ass2/q3.hs
+++ ass2/q3.hs
@@ -5,7 +5,5 @@ errno = length . unbalanced
 -- be removed to obtain a balanced parentheses string.
 unbalanced :: String -> String
 unbalanced = foldl f []
-  where f acc x
-          | null acc = x:acc
-          | (head acc, x) == ('(', ')') = tail acc
-          | otherwise = x:acc
+  where f ('(':xs) ')' = xs
+        f xs x = x:xs
